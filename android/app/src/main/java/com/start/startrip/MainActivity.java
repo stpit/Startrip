ae0b8f838a507a4ddee842fdc12ad4e4cd65c9c9
stpit <sypss9990@gmail.com>
2016-09-08 16:58:57 +0800

    new

---
+++ android/app/src/main/java/com/start/startrip/MainActivity.java
@@ -0,0 +1,17 @@
+package com.start.startrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
